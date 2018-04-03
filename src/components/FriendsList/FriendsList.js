24bc0c610a7d9b8ea020047e4d1ed53b36d431a4
tmat06 <timmy.a.matthews@gmail.com>
2018-04-03 14:10:56 -0600

    Nav on all Components needed

--- src/components/FriendsList/FriendsList.js
+++ src/components/FriendsList/FriendsList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Nav from './../Nav/Nav';
 
 export default function FriendsList() {
     return(
         <div>
+            <Nav />
             FriendsList
             <Link to='/dashboard'><button>Dashboard</button></Link>
         </div>
--- src/components/GoogleDirections/GoogleDirections.js
+++ src/components/GoogleDirections/GoogleDirections.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Nav from './../Nav/Nav';
 
 export default function GoogleDirections(){
     return (
         <div>
+            <Nav />
             Google Biz Right here
             <Link to='/spin-results'><button>Spin Results</button></Link>
         </div>
