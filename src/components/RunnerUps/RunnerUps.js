ccef1b8abc835801e32b7d1bc8bb7f88f072dab4
tmat06 <timmy.a.matthews@gmail.com>
2018-04-13 17:07:10 -0600

    runner-ups is now the randomizer, spin-results will display restaurants where they can choose which restaurants will be in the spin. all appears to be working except the delete wont re render as well as change name

--- src/components/RunnerUps/RunnerUps.js
+++ src/components/RunnerUps/RunnerUps.js
@@ -1,13 +1,54 @@
 import React from 'react';
 import Nav from './../Nav/Nav';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default function RunnerUps(){
-    return(
+function RunnerUps(props) {
+
+    console.log('runnerUps', props.currentList)
+
+    var currentList = [];
+    let randomSpot = 0;
+
+    for (let i = 0; i < props.currentList.length; i++) {
+        currentList[i] = "";
+    }
+
+    for (var i = 0; i < props.currentList.length; i++) {
+        randomSpot = Math.floor(Math.random() * props.currentList.length);
+        while (currentList[randomSpot]) {
+            randomSpot = Math.floor(Math.random() * props.currentList.length);
+        }
+        var inputStudent = props.currentList[i];
+        currentList[randomSpot] = inputStudent;
+    }
+
+    return (
         <div>
             <Nav />
             RunnerUps
             <Link to='/spin-results'><button>Spin Results</button></Link>
+            {currentList.map((val, i) => {
+                console.log(val)
+                if (i === 0) {
+                    return (
+                        <div>Winner is: {val.name}</div>
+                    )
+                } else {
+                    return (
+                        <div key={i}>{val.name}</div>
+                    )
+
+                }
+            })}
         </div>
     )
 }
+
+function mapStateToProps(state) {
+    return {
+        currentList: state.restaurantList
+    }
+}
+
+export default connect(mapStateToProps)(RunnerUps);
